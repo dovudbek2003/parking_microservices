88545af94b49fb9c13c84c2efe9a9b4c6ff22214
Dovudbek <oktamovdovudbek030415@gmail.com>
2024-05-04 20:28:53 +0500

    park service fayil structurasi tayor

--- park/src/app.module.ts
+++ park/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ParkModule } from './modules/park/park.module';
 import { LayerModule } from './modules/layer/layer.module';
 import { PlaceModule } from './modules/place/place.module';
@@ -8,8 +6,6 @@ import { TariffModule } from './modules/tariff/tariff.module';
 import { ServiceModule } from './modules/service/service.module';
 
 @Module({
-  imports: [ParkModule, LayerModule, PlaceModule, TariffModule, ServiceModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ParkModule, LayerModule, PlaceModule, TariffModule, ServiceModule]
 })
-export class AppModule {}
+export class AppModule { }
